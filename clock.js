252ea9c16fd4030275dc86124083f1c5b7817290
ahmedCS09 <musabahmed305@gmail.com>
2025-04-16 18:45:59 +0500

    my clock

---
+++ clock.js
@@ -0,0 +1,25 @@
+function displayTime()
+{
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+
+    if (hrs >= 12)
+    {
+        session.innerHTML = "PM";
+    }
+    else{
+        session.innerHTML = "AM";
+    }
+    if (hrs === 0) {
+        hrs = 12;
+    } else if (hrs > 12) {
+        hrs = hrs - 12;
+    }
+
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("minutes").innerHTML = min;
+    document.getElementById("seconds").innerHTML = sec;
+}
+setInterval(displayTime, 1000);
